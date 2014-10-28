0e778f8ea8705cad7116d63aa8d175fcea124679
Jur Balledux <j.balledux@simgroep.nl>
2014-10-28 12:18:29 +0100

    Fixed single provisioning setup

--- provisioning/local.sh
+++ provisioning/local.sh
@@ -10,4 +10,4 @@ pip -q install ansible
 cp -f /vagrant/provisioning/hosts /home/vagrant/hosts
 chmod 666 /home/vagrant/hosts
 export PYTHONUNBUFFERED=1
-su vagrant -c "ansible-playbook /vagrant/provisioning/playbook.yml -i /home/vagrant/hosts --connection=local --limit 'development:&local'"
+su vagrant -c "ansible-playbook /vagrant/provisioning/playbook.yml -i /home/vagrant/hosts --connection=local --limit 'development'"
