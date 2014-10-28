39f589320d5414cf24f9b3f9ee28b00685cffc8d
Jur Balledux <j.balledux@simgroep.nl>
2014-10-28 12:18:09 +0100

    Initial bundle setup

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,11 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
+            
+            new Mhwk\MediaPlayerBundle\MhwkMediaPlayerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Mhwk/MediaPlayerBundle/MhwkMediaPlayerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mhwk\MediaPlayerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MhwkMediaPlayerBundle extends Bundle
+{
+}
